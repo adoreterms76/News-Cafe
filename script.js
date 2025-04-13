5ffb6ecff5b1d0744f7d8e6694e6eb321ff8864e
adoreterms76 <boityboitumelotlhaselo@gmail.com>
2025-04-14 00:32:18 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function nextSlide(id) {
+  const container = document.getElementById(id);
+  const slides = container.querySelectorAll(".slide");
+  let current = Array.from(slides).findIndex(slide => slide.classList.contains("active"));
+  slides[current].classList.remove("active");
+  let nextIndex = (current + 1) % slides.length;
+  slides[nextIndex].classList.add("active");
+}
+
+function prevSlide(id) {
+  const container = document.getElementById(id);
+  const slides = container.querySelectorAll(".slide");
+  let current = Array.from(slides).findIndex(slide => slide.classList.contains("active"));
+  slides[current].classList.remove("active");
+  let prevIndex = (current - 1 + slides.length) % slides.length;
+  slides[prevIndex].classList.add("active");
+}
