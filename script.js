5caf5423dd8a24b811fe07259becdbb333e423cc
adoreterms76 <boityboitumelotlhaselo@gmail.com>
2025-04-16 05:38:04 +0200

    script.js

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
 // Index Layout
-// Function to switch slides
+// script.js
+
 function nextSlide(slideshowId) {
   const slideshow = document.getElementById(slideshowId);
-  const slides = slideshow.querySelectorAll(".slide");
-  let currentIndex = -1;
+  const slides = slideshow.querySelectorAll('.slide');
+  let index = Array.from(slides).findIndex(slide => slide.classList.contains('active'));
 
-  slides.forEach((slide, index) => {
-    if (slide.classList.contains("active")) {
-      currentIndex = index;
-      slide.classList.remove("active");
-    }
-  });
-
-  const nextIndex = (currentIndex + 1) % slides.length;
-  slides[nextIndex].classList.add("active");
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
 }
 
 function prevSlide(slideshowId) {
   const slideshow = document.getElementById(slideshowId);
-  const slides = slideshow.querySelectorAll(".slide");
-  let currentIndex = -1;
+  const slides = slideshow.querySelectorAll('.slide');
+  let index = Array.from(slides).findIndex(slide => slide.classList.contains('active'));
 
-  slides.forEach((slide, index) => {
-    if (slide.classList.contains("active")) {
-      currentIndex = index;
-      slide.classList.remove("active");
-    }
-  });
-
-  const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
-  slides[prevIndex].classList.add("active");
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
 }
+
 
 // Breakfast Layout
 // Initialize slide index
