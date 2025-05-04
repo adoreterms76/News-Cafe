aaca42d46d523d186e611d8046aad2276679f30b
adoreterms76 <boityboitumelotlhaselo@gmail.com>
2025-05-04 03:03:04 +0200

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // contact layout
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('.contact-form');
+  const thankYouMessage = document.querySelector('.thank-you-message');
+
   form.addEventListener('submit', function (e) {
-    e.preventDefault(); // stops the page from refreshing
-    alert('Thank you! Your message has been sent.');
-    form.reset(); // clear the form
+    e.preventDefault(); // prevent page reload
+    form.style.display = 'none'; // hide form
+    thankYouMessage.style.display = 'block'; // show thank-you message
   });
 });
 
