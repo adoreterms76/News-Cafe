1e9ec5360ad4def20bab4b41f9848ab1c7d0a0bb
adoreterms76 <boityboitumelotlhaselo@gmail.com>
2025-05-06 23:38:31 +0200

    script.js

--- script.js
+++ script.js
@@ -12,5 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
     // Hide the form and show thank-you message
     form.style.display = "none";
     thankYouMessage.style.display = "block";
+
+function showThankYouMessage() {
+  event.preventDefault(); // Stop the form from submitting
+  document.getElementById("contactForm").style.display = "none";
+  document.querySelector(".thank-you-message").style.display = "block";
+  return false; // Prevent form refresh
+}
+    
   });
 });
