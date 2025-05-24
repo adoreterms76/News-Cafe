43c60e8599136998bf14f03ceb2cd7d18fa0f30a
adoreterms76 <boityboitumelotlhaselo@gmail.com>
2025-05-24 20:22:32 +0200

    script.js

--- script.js
+++ script.js
@@ -14,22 +14,3 @@ function showThankYouMessage(event) {
   });
   });
 
-// download as pdf
-<script>
-  document.getElementById('downloadPdf').addEventListener('click', function () {
-    const { jsPDF } = window.jspdf;
-
-    html2canvas(document.querySelector("#menu-images")).then(canvas => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF("p", "mm", "a4");
-
-      // Calculate width and height to fit A4
-      const imgProps = pdf.getImageProperties(imgData);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-      pdf.save("news-cafe-menu.pdf");
-    });
-  });
-
